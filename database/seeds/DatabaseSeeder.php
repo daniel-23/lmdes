1b7d27ff7b34749ac00e1adffca4cff78df61a48
daniel-23 <danielbatlet@gmail.com>
2020-08-15 08:28:42 -0500

    Descomentar Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        /*$this->call([
+        $this->call([
             ComponentsTableSeeder::class,
             PermissionsSeeder::class,
             RoleTableSeeder::class,
@@ -28,7 +28,6 @@ class DatabaseSeeder extends Seeder
             CurrenciesSeeder::class,
             LanguagesSeeder::class,
             ResourcesSeeder::class
-        ]);*/
-        $this->call(ResourcesSeeder::class);
+        ]);
     }
 }
